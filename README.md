0419733925104806037df390e33985877e5bf01d
Minzkraut <git@minzkraut.com>
2022-04-17 16:39:56 +0200

    Configure sequelize using config.js and sequelizerc

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ```bash
 $ docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
 ```
+
+### Sequelize migrations
+
+If ran outside the container, DB_HOST must be set to localhost or the container's IP address.
+
+```bash
+$npx sequelize db:migrate
+``` 
--- models/index.js
+++ models/index.js
@@ -1,18 +1,19 @@
 'use strict';
 
-require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  port: 3306
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
