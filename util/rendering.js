31b2836a45a4344adb58307133007dccee3e93c8
Minzkraut <git@minzkraut.com>
2022-08-29 00:40:41 +0200

    Render: Remove default BG color from card stacks

--- util/rendering.js
+++ util/rendering.js
@@ -27,7 +27,7 @@ module.exports = {
                 width: 900,
                 height: 500,
                 channels: 4,
-                background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+                background: { r: 0, g: 0, b: 0, alpha: 0.0 },
                 animated: true
             }
          }).composite([
