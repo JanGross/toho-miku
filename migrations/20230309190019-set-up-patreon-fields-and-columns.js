cb01327c7d5cd32e51f307abc3ce283d7bf2bb44
Minzkraut <git@minzkraut.com>
2023-03-12 17:41:21 +0100

    DB: Change patreonTierRoles column to type STRING

--- migrations/20230309190019-set-up-patreon-fields-and-columns.js
+++ migrations/20230309190019-set-up-patreon-fields-and-columns.js
@@ -29,7 +29,7 @@ module.exports = {
       defaultValue: 1
     });
     await queryInterface.addColumn('Bots', 'patreonTierRoles', {
-      type: Sequelize.INTEGER,
+      type: Sequelize.STRING,
       defaultValue: 1
     });
   },
